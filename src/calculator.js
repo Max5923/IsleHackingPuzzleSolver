03ceab9a47d171c9c80b7db9f1acbeeb84b46612
Max5923 <maxzumi441@gmail.com>
2024-09-23 19:43:00 -0400

    Getting closer to a formula finder for A.

--- src/calculator.js
+++ src/calculator.js
@@ -1,5 +1,6 @@
 import {evaluate} from 'mathjs'
 import * as regmath from 'regression'
+import { arrayBuffer } from 'stream/consumers'
 
 const number1 = document.getElementById("number1")
 const number2 = document.getElementById("number2")
@@ -13,22 +14,36 @@ const formula2 = document.getElementById("formula2")
 const devoutput = document.getElementById("devoutput")
 
 var data = [];
+var regdata = [];
 var questionnum = [0, 0, 0, 0];
 
+var tempdata = new Array(10);
 function ProcessNumbers(){
-
     let num1 = parseInt(number1.value);
     let num2 = parseInt(number2.value);
 
-    data.push([questionnum[0], num1]);
-    //data.push([[i, 0, 0, 0], [num1, num2]]);
+    data.push([questionnum, [num1, num2]]);
+    tempdata[questionnum[0]] = num1;
+    regdata.push([questionnum[0], num1]);
 
+    let regformula = regmath.polynomial(data, {order: questionnum[0] - 1}).string;
+    let invalidation = false;
     if (questionnum[0] > 0){
-        formula1.textContent = regmath.polynomial(data, {order: questionnum[0] - 1}).string;
+        
+        for (let i = 0; i < questionnum[0] | invalidation; i++){
+            if(evaluate(regformula, {x: i}) != tempdata[i]){
+                invalidation = true;
+            }
+        }
     }
 
-    questionnum[0]++;
-    questionout.textContent = "Enter " + questionnum.toString();
+    if(invalidation){
+        questionnum[0]++;
+        questionout.textContent = "Enter " + questionnum.toString();
+    } else{
+        questionout.textContent = regformula;
+    }
+
 }
 inputbutton.addEventListener("click", ProcessNumbers);
 
